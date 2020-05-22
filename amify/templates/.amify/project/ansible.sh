d337480d4fbef926921a03d5641841ac73358718
Dan Bowen <dan@crucialwebstudio.com>
2020-05-21 22:17:32 -0500

    Refactor template structure

--- amify/templates/.amify/project/ansible.sh
+++ amify/templates/.amify/project/ansible.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# TODO delete this and call ansible shell via python
 ansible-playbook {{ original_playbook_name }}.amfy.{{ project_name }}.yml \
     --inventory={{ original_playbook_name }}.amfy.{{ project_name }}.ini \
     --ssh-common-args='-F ./.amify/ssh.cfg';
--- example/.amify/ansible.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-cd ../../
-ansible-playbook ami-creator.sample-project-dev.playbook.yml \
-    --inventory=ami-creator.sample-project-dev.inventory.ini \
-    --ssh-common-args='-F ./ami-creator/sample-project-dev/ssh.cfg';
